f5e1dedbc0ba416f80b861e142780bc9f39a6504
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-04-02 13:01:37 -0700

    First Step

---
+++ ACMHack/HomePage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ACMHack
+{
+	public class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			var button = new Button () {
+				Text = "Click me!",
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.End
+			};
+
+			var label = new Label () {
+				Text = "Hello world!",
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			button.Clicked += (object sender, EventArgs e) => {
+				label.Text = "I changed it!";
+			};
+
+			Content = new StackLayout { 
+				Children = {
+					label, button
+				},
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Thickness(20)
+			};
+		}
+	}
+}
+
+
